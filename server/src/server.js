4e6f6983af8c9729ddf9ca1a4adc273b5ce84d80
kobbi1 <jakob-daniel@hotmail.com>
2025-04-14 19:01:13 +0000

    added vercel to allowedOrigins

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 3000
 
 const allowedOrigins = [
     "http://localhost:3000",
-    "https://einstaklingsverkefni-95p0.onrender.com"
+    "https://einstaklingsverkefni-95p0.onrender.com",
+    "https://einstaklingsverkefni-six.vercel.app"
   ];
 
 app.use(
